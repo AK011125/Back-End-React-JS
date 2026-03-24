ab475dfbd44fc22b8a6160c84ad38775445955ea
ak011125 <ajaykumarjakhar08@gmail.com>
2026-03-24 19:30:08 +0530

    9th commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from  './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+    <Card />
     </>
   )
 }
