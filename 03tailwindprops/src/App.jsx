695df3cc52f9281d7088639cec369db1458170ca
ak011125 <ajaykumarjakhar08@gmail.com>
2026-03-25 00:20:41 +0530

    10th commit Tailwindcss and props basic

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -11,7 +11,8 @@ function App() {
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
-    <Card />
+    <Card username="Mr AK Jakhar" btnText="Share"/>
+    <Card username="Er AK Jakhar"/>
     </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({username , btnText ="Price" }) {
+    console.log(username);
   return (
     <div
       className="flex flex-col rounded-xl  p-4"
@@ -22,8 +23,8 @@ export default function Card() {
       </div>
       <div className="flex flex-col  rounded-b-xl py-4 ">
         <div className="flex justify-between">
-          <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
-          <h1 className="font-bold font-RubikBold">Price</h1>
+          <h1 className="font-RubikBold ">{username}</h1>
+          <h1 className="font-bold font-RubikBold">{btnText}</h1>
         </div>
         <div className="flex  justify-between font-mono">
           <p>#345</p>
